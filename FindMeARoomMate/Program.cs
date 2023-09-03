91e4daf00ed0a5d9a0292806c51caac5e0a74458
AxelShehaj <aksel1996@hotmail.com>
2023-09-03 10:12:00 +0200

    new

--- FindMeARoomMate/Program.cs
+++ FindMeARoomMate/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Presentation Layer
+using FindMeARoomMate.DataLayer.DBContext;
+using FindMeARoomMate.DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello");
+
+            FindMeRoomMateDBContext findMeRoomMateDBCotext  = new FindMeRoomMateDBContext();
+            findMeRoomMateDBCotext.Add(new Students() { FullName = "Aksel Shehaj" });
+            await findMeRoomMateDBCotext.SaveChangesAsync();
+
+            var student = await findMeRoomMateDBCotext.Students.ToListAsync();
+            student.ForEach(student => { Console.WriteLine(student.FullName); });
+
+
+var deleteStudent = await findMeRoomMateDBCotext.Students.Where(s => s.Id == 10).FirstOrDefaultAsync();
+if (deleteStudent == null)
+{
+    throw new Exception("Studnet not found");
+}
+findMeRoomMateDBCotext.Students.Remove(deleteStudent);
+await findMeRoomMateDBCotext.SaveChangesAsync();
+
